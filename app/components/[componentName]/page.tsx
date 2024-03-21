459a9c019c9dbcf6eb1c46099c9d9e9c777b0f7b
daoodaba975 <davinhoba9@outlook.fr>
2024-03-21 22:38:01 +0000

    [feat]: wire Preview & Code on pannel.

--- app/components/[componentName]/page.tsx
+++ app/components/[componentName]/page.tsx
@@ -4,7 +4,7 @@ import ErrorMessage from "@/components/Error/ErrorMessage";
 import UIComponentPreview from "@/components/UIComponentPreview";
 import ComponentDetails from "@/components/galsenUiComponents/ComponentDetails";
 
-import { promises as fs } from 'fs';
+import { promises as fs } from "fs";
 
 type PageProps = {
   params: { componentName: string };
@@ -14,45 +14,72 @@ type PageProps = {
 export default async function Page({ params }: PageProps) {
   /**
    * Read all the files located inside "componentName" folder
-   * 
+   *
    * For example, "componentName" can be "forms" folder located inside public/ui
    */
-  let files
+  let files;
   try {
-    files = await fs.readdir(process.cwd() + `/public/ui/${params.componentName}/`, 'utf8');
+    files = await fs.readdir(
+      process.cwd() + `/public/ui/${params.componentName}/`,
+      "utf8"
+    );
   } catch (error) {
-    return <ErrorMessage message="Veuillez verifier si cette catégorie de composants existe" />
+    return (
+      <ErrorMessage message="Veuillez verifier si cette catégorie de composants existe" />
+    );
   }
 
   // Get only html files so that we can display the code
-  const htmlFiles = files.filter(file => file.endsWith(".html"))
+  const htmlFiles = files.filter((file) => file.endsWith(".html"));
 
   // The description of each component is located in a file with ".txt" extension
-  const componentDescriptionFile = files.find(file => file.endsWith(".txt"))
+  const componentDescriptionFile = files.find((file) => file.endsWith(".txt"));
 
   // Now we can read the content inside the component description file
-  const componentDescription = await fs.readFile(process.cwd() + `/public/ui/${params.componentName}/${componentDescriptionFile}`, 'utf8')
+  const componentDescription = await fs.readFile(
+    process.cwd() +
+      `/public/ui/${params.componentName}/${componentDescriptionFile}`,
+    "utf8"
+  );
 
   return (
     <main className="">
       <section className="px-4 py-16 sm:max-w-7xl sm:mx-auto">
-        <h1 className="text-2xl font-bold capitalize">{params.componentName}</h1>
-        <p className="text-neutral-500">
-          {componentDescription}
-        </p>
+        <h1 className="text-2xl font-bold capitalize">
+          {params.componentName}
+        </h1>
+        <p className="text-neutral-500">{componentDescription}</p>
 
         {/* TODO: expose the component name as a prop */}
-        {
-          htmlFiles.length ?
-            htmlFiles
-              .map((file: string) => <UIComponentPreview key={file} category={params.componentName} file={file} />) :
-            <p>Empty</p>
-        }
+        {/* {htmlFiles.length ? (
+          htmlFiles.map((file: string) => (
+            <UIComponentPreview
+              key={file}
+              category={params.componentName}
+              file={file}
+            />
+          ))
+        ) : (
+          <p>Empty</p>
+        )} */}
+
+        {htmlFiles.length ? (
+          htmlFiles.map((file: string) => (
+            <ComponentDetails
+              title="Bouton Simple"
+              key={file}
+              category={params.componentName}
+              file={file}
+            />
+          ))
+        ) : (
+          <p>Empty</p>
+        )}
 
         {/* TODO: remove the `hidden` class */}
         <div className="mt-16 space-y-12 hidden">
-          <ComponentDetails title="Bouton Simple" />
-          <ComponentDetails title="Bouton Pas Simple" />
+          {/* <ComponentDetails title="Bouton Simple" />
+          <ComponentDetails title="Bouton Pas Simple" /> */}
         </div>
       </section>
     </main>
--- components/galsenUiComponents/ComponentDetails.tsx
+++ components/galsenUiComponents/ComponentDetails.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { componentPreviewHtml } from "@/utils/transformers";
 
-type ComponentDetailsProps = {
+type PropsType = {
   title: string;
+  file: string;
+  category: string;
 };
 
-const ComponentDetails = ({ title }: ComponentDetailsProps) => {
+const ComponentDetails = ({ title, file, category }: PropsType) => {
   const [tab, setTab] = useState<"preview" | "code">("preview");
+
+  const [code, setCode] = useState<string | undefined>("");
+  useEffect(() => {
+    fetchUiComponent();
+  });
+
+  async function fetchUiComponent() {
+    const res = await fetch(`/ui/${category}/${file}`);
+    setCode(await res.text());
+  }
 
   return (
     <article className="grid grid-cols-[auto_auto] gap-y-4 justify-between items-center">
@@ -17,27 +30,48 @@ const ComponentDetails = ({ title }: ComponentDetailsProps) => {
         <button
           onClick={() => setTab("preview")}
           type="button"
-          className={`py-2 px-3 rounded ${tab === "preview" ? "bg-neutral-200" : "bg-transparent"}`}
+          className={`py-2 px-3 rounded ${
+            tab === "preview" ? "bg-neutral-200" : "bg-transparent"
+          }`}
         >
           Aperçu
         </button>
         <button
           onClick={() => setTab("code")}
           type="button"
-          className={`py-2 px-3 rounded ${tab === "code" ? "bg-neutral-200" : "bg-transparent"}`}
+          className={`py-2 px-3 rounded ${
+            tab === "code" ? "bg-neutral-200" : "bg-transparent"
+          }`}
         >
           Code
         </button>
       </div>
 
       {/* TODO: use aspect-ratio to center the handle */}
+      {/* // TODO: work on this to have a better display  */}
       <div className="col-span-full h-[486px]">
-        {tab === "preview" ? <TabPreview /> : <TabCode />}
+        {tab === "preview" ? (
+          <article className="border-l pl-2 overflow-y-auto space-y-4">
+            {code ? (
+              <iframe
+                className="w-full h-screen"
+                srcDoc={componentPreviewHtml(code)}
+              ></iframe>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </article>
+        ) : (
+          <div className="w-full h-full flex items-center justify-center bg-neutral-300 rounded-lg">
+            {code ? <pre>{code}</pre> : <p>Loading...</p>}
+          </div>
+        )}
       </div>
     </article>
   );
 };
 
+// TODO: need to use child components for Preview and Code
 const TabPreview = () => {
   return (
     <PanelGroup
