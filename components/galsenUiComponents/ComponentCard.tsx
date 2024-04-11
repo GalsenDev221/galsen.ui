3b21e853884349a7063b7f41a5b4a55677bffcab
daoodaba975 <davinhoba9@outlook.fr>
2024-04-11 19:27:16 +0000

    [feat]: display components from homepage.

--- components/galsenUiComponents/ComponentCard.tsx
+++ components/galsenUiComponents/ComponentCard.tsx
@@ -1,17 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { IComponent } from '@/utils/components'
 
-const ComponentCard: React.FC<IComponent> = ({ image, name, count, alt }) => {
+const ComponentCard: React.FC<IComponent> = ({ image, name, count, alt, link }) => {
     return (
         <div className='w-full'>
             <div className='w-full h-40 relative'>
-                <Image
-                    src={image}
-                    alt={alt}
-                    fill={true}
-                    className='rounded-md shadow-sm border object-contain'
-                />
+                <Link href={link} className='w-full h-full rounded-md shadow-sm border object-contain'>
+                    <Image
+                        src={image}
+                        alt={alt}
+                        fill={true}
+                        className='rounded-md shadow-sm border object-contain'
+                    />
+                </Link>
             </div>
             <h3 className='text-xl mt-3'>{name}</h3>
             <span className='text-sm text-cardDesc'>{count} composants</span>
