b7cd3897b2859b242b6b9b5bb36806ea07044f83
daoodaba975 <davinhoba9@outlook.fr>
2024-11-29 22:19:00 +0000

    [FIX]: Remove old `component` preview  🐞

---
+++ utils/components.ts
@@ -0,0 +1,51 @@
+export interface IComponent {
+  image: string;
+  name: string;
+  count: number;
+  alt: string;
+  link: string;
+}
+export const componentsFakeData: IComponent[] = [
+  {
+    image: "/images/tables.png",
+    name: "Tables",
+    count: 3,
+    alt: "Tables components",
+    link: "/components/tables",
+  },
+  {
+    image: "/images/navbars.png",
+    name: "Navbars",
+    count: 3,
+    alt: "Navbars components",
+    link: "/components/navbars",
+  },
+  {
+    image: "/images/cards.png",
+    name: "Cards",
+    count: 3,
+    alt: "Cards components",
+    link: "/components/cards",
+  },
+  {
+    image: "/images/footers.png",
+    name: "Footers",
+    count: 2,
+    alt: "Footers components",
+    link: "/components/footers",
+  },
+  {
+    image: "/images/forms.png",
+    name: "Forms",
+    count: 3,
+    alt: "Forms components",
+    link: "/components/forms",
+  },
+  {
+    image: "/images/buttons.png",
+    name: "Buttons",
+    count: 2,
+    alt: "buttons components",
+    link: "/components/buttons",
+  },
+];
